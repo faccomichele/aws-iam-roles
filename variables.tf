7292fd1bd0ae02dfdfacc4163b668fbaa4354fdb
Michele Facco <faccomichele@gmail.com>
2026-07-20 23:37:34 +0700

    test change (#26)

--- variables.tf
+++ variables.tf
@@ -8,25 +8,3 @@ variable "gha-roles" {
   type        = list(string)
   default     = []
 }
-
-# variable "github_repository_id" {
-#   description = "GitHub repository ID from Actions context (for example: ${{ github.repository_id }})"
-#   type        = string
-#   default     = null
-
-#   validation {
-#     condition     = var.github_repository_id == null || can(regex("^[0-9]+$", var.github_repository_id))
-#     error_message = "github_repository_id must be null or a numeric string."
-#   }
-# }
-
-# variable "github_actor_id" {
-#   description = "GitHub actor ID from Actions context (for example: ${{ github.actor_id }})"
-#   type        = string
-#   default     = null
-
-#   validation {
-#     condition     = var.github_actor_id == null || can(regex("^[0-9]+$", var.github_actor_id))
-#     error_message = "github_actor_id must be null or a numeric string."
-#   }
-# }
